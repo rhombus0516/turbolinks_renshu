e450038c12fb6c8d7dbbe47d4e1ecb49a0f18cbf
rhombus0516 <karaya0913@gmail.com>
2023-12-04 00:29:14 +0000

    [Add]入力ダイアログで値を入力

--- app/javascript/app.js
+++ app/javascript/app.js
@@ -1,11 +1,14 @@
 //let promptStr = prompt('何か好きな文字を入力してください。');
 //alert(promptStr);
 
-let alertStering;
-alertStering = addString("webcamp");
-alert(alertStering);
+//let alertStering;
+//alertStering = addString("webcamp");
+//alert(alertStering);
 
-function addString(strA){
-    let addStr = "Hello" + strA;
-    return addStr;
-}
+//function addString(strA){
+    //let addStr = "Hello" + strA;
+    //return addStr;
+//}
+
+let promptStr = prompt('好きな文字');
+alert(promptStr);
