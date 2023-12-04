8a60785ef022209e6cf51d911f2a6f0a89246e7d
rhombus0516 <karaya0913@gmail.com>
2023-12-04 00:25:47 +0000

    [Add]関数

--- app/javascript/app.js
+++ app/javascript/app.js
@@ -1,3 +1,11 @@
-let promptStr = prompt('何か好きな文字を入力してください。');
+//let promptStr = prompt('何か好きな文字を入力してください。');
+//alert(promptStr);
 
-alert(promptStr);
+let alertStering;
+alertStering = addString("webcamp");
+alert(alertStering);
+
+function addString(strA){
+    let addStr = "Hello" + strA;
+    return addStr;
+}
